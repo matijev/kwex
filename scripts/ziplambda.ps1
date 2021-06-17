bf81bb81e68fc8cc25b8b2993a999c7f59a897bf
Matijevic, Vladimir <Vladimir.Matijevic@fisglobal.com>
2021-06-17 16:28:36 +0200

    install

--- scripts/ziplambda.ps1
+++ scripts/ziplambda.ps1
@@ -1,3 +1,5 @@
 $CurrentDir = $(get-location).Path;
 echo $CurrentDir
+dotnet tool install --global Amazon.Lambda.Tools --version 3.1.1
+dotnet tool update -g Amazon.Lambda.Tools
 dotnet lambda package -c Release -o ../../publish/ce_pipeline_lambda.zip
